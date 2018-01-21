56b86d599486fb3e3162b229570159a02a34f71d
Karol Kotowicz <kkotowiczz@gmail.com>
2018-01-21 11:57:51 +0100

    added appending data to paragraphs

--- src/app/App.js
+++ src/app/App.js
@@ -11,6 +11,7 @@ const App = () => {
             <Navbar
             />
             <CountryStats />
+            {/*<Heroes />*/}
         </div>
 
     )
--- src/app/components/CountryStats.js
+++ src/app/components/CountryStats.js
@@ -1,20 +1,21 @@
 // https://api.opendota.com/api/distributions / country_mmr
 
 import React, {Component} from "react";
-import d3 from 'd3';
+import * as d3 from 'd3';
 
 class CountryStats extends Component {
     state = {
       countriesList: []
     };
 
-    componentDidMount() {
+    componentWillMount() {
         const REQ_URL = 'https://api.opendota.com/api/distributions';
         const REQ_OPT = {
             method: "GET",
             mode: "cors"
         };
         fetch(REQ_URL, REQ_OPT).then(res => res.json()).then(countriesData => {
+            console.log(countriesData);
             this.setState(() => {
                 return {countriesList: countriesData}
             });
@@ -22,12 +23,21 @@ class CountryStats extends Component {
 
             })
         })
+
     }
+    componentDidUpdate() {
+        d3.select("#chart1").selectAll("p").data(this.state.countriesList.country_mmr.rows).enter().append("p").text((d, i) => JSON.stringify(d, undefined, 2))
+    }
+    createChart = () => {
+
+    };
 
     render() {
         return (
             <div>
+                <div id="chart1">
 
+                </div>
             </div>
         )
     }
