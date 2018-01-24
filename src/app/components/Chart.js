24a4eb5cbffcfad3704cf7fb16bce2ac75a1649e
Karol Kotowicz <kkotowiczz@gmail.com>
2018-01-24 20:06:17 +0100

    fixed world map

--- src/app/components/Chart.js
+++ src/app/components/Chart.js
@@ -3,36 +3,43 @@ import * as d3 from 'd3'
 import * as topoJSON from 'topojson'
 
 class Chart extends Component {
-    createChart () {
-        let width = 900;
-        let height = 600;
-
-        let projection = d3.geoMercator();
-
-        let svg = d3.select("#svgContainer").append("svg")
-            .attr("width", width)
-            .attr("height", height);
-        let path = d3.geoPath()
-            .projection(projection);
-        let g = svg.append("g");
-
-        d3.json("https://unpkg.com/world-atlas@1.1.4/world/110m.json", (error, topology) => {
-            g.selectAll("path")
-                .data(topoJSON.feature(topology, topology.objects.countries)
-                    .features)
-                .enter()
-                .append("path")
-                .attr("d", path)
-        });
+    state = {
+        worldData: []
+    };
+    projection() {
+        return d3.geoMercator()
+            .scale(100)
+            .translate([800/2, 450/2])
     }
     componentDidMount() {
-        this.createChart()
+        fetch("https://unpkg.com/world-atlas@1/world/110m.json")
+            .then(res => {
+                if(res.status !== 200) {
+                    console.log(`There was a problem: ${res.status}`)
+                    return
+                }
+                res.json().then(worldData=> {
+                    this.setState({
+                        worldData: topoJSON.feature(worldData, worldData.objects.countries).features
+                    })
+                })
+            })
     }
-
     render() {
         return (
-            <div id="svgContainer">
-            </div>
+            <svg width={800} height={450} viewBox="0 0 800 450">
+                <g className="countries">
+                    {
+                        this.state.worldData.map((d,i) => (
+                            <path
+                                key={`path-${i}`}
+                                d={d3.geoPath().projection(this.projection())(d)}
+                                className="country"
+                            />
+                        ))
+                    }
+                </g>
+            </svg>
         )
     }
 }
