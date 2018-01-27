04abb3d055e17500f2257ad1de7fc5d0b92f4679
Karol Kotowicz <kkotowiczz@gmail.com>
2018-01-27 12:56:34 +0100

    refactored hero card rendering

--- src/app/components/HeroCard.js
+++ src/app/components/HeroCard.js
@@ -1,41 +1,40 @@
 import React from 'react';
-
-
 const HeroCard = props => {
+    const attributes = ['str', 'agi', 'int']
     const REQ_URL = `https://api.opendota.com`;
     const REQ_OPTIONS = {
         method: "GET",
         mode: "cors"
     };
-
     const handleClick = () => {
+        let heroData;
         fetch(`${REQ_URL}/api/heroes/${props.id}/matches`, REQ_OPTIONS)
             .then(res => res.json())
             .then(heroMatches => {
-                const heroData = heroMatches.map(match => ({kills: match.kills, deaths: match.deaths, assists: match.assists}))
+                heroData = heroMatches.map(match => ({kills: match.kills, deaths: match.deaths, assists: match.assists}))
                     .reduce((acc, val) => ({kills: acc.kills + val.kills, deaths: acc.deaths + val.deaths, assists: acc.assists + val.assists}))
-                console.log(heroData)
+                console.log(heroData, props.id, props.name)
             })
     };
-
     return (
       <div className='heroCard' >
           <div className="heroCardWrapper" onClick={handleClick}>
               <img src={props.avatarURL} className="avatarImg" />
               <figcaption>{props.name}</figcaption>
-              <div className='attributesRow'>
-                  <div className={props.mainAttr === 'str' ? "prim attr" : "attr"}>
-                      <img src="../assets/images/Strength_Icon.png" />
-                      {props.str}
-                  </div>
-                <div className={props.mainAttr === 'agi' ? "prim attr" : "attr"}>
-                    <img src="../assets/images/Agility_Icon.png" />
-                    {props.agi}
-                </div>
-                <div className={props.mainAttr === 'int' ? "prim attr" : "attr"}>
-                    <img src="../assets/images/Intelligence_Icon.png" />
-                    {props.int}
-                </div>
+              <div className="attributesRow">
+              {
+                attributes.map((attr, index) => {
+                    return (
+                            <div
+                                className={props.mainAttr === attr ? "prim attr" : "attr"}
+                                key={index}
+                            >
+                                <img src={`../assets/images/${attr}.png`} />
+                                {props[attr]}
+                            </div>
+                    )
+                })
+              }
               </div>
           </div>
       </div>
